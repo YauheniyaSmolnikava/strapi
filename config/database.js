b488772b24ee970a1d94089cea899a8229259111
Yauheniya Smolnikava <Yauheniya_Smolnikava@epam.com>
2021-04-23 09:52:15 +0300

    updated config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+   "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "mongoose",
+      "settings": {
+        "client": "mongo",
+        "host": "test-strapi-cosmos.mongo.cosmos.azure.com",
+        "port": 10255,
+        "database": "test-strapi-cosmos",
+        "username": "test-strapi-cosmos",
+        "password": "7I9JQuF66UL1K10w1vK19tIgL8Vr1mrs9qBn56jHq712bjM9Btf7xARLQqS1XQq8Up4dPvowqeSs1XdawYf7mg=="
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+      "options": {
+		  "authenticationDatabase": "test-strapi-cosmos",
+        "ssl": true
+	  }
+    }
+  }
 });
--- config/server.js
+++ config/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '8313c1852450522bc605751442c38931'),
-    },
-  },
+  host: "${process.env.HOST}",
+  port: "${process.env.PORT || 1337}"
 });
